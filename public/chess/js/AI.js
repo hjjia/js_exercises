3526ba62952a8d2888b2973bc17d9ed9bb464bf8
黄佳佳 <normal999@163.com>
2016-04-01 11:57:36 +0800

    五指棋AI篇 -> ai

--- public/chess/js/AI.js
+++ public/chess/js/AI.js
@@ -229,67 +229,240 @@ AI.getPosition = function(direction,x,y){
 
 // 连子数
 AI.getChessNum = function (x,y) {
-    var type = 0
-    type = chessBoardArr[x][y]
+    var point     = this
+    point.len     = chessBoardArr.length
+    point.type    = chessBoardArr[x][y]
 
-    // 从八个方向来判断连续的数量 1 top 2 bottom 3 left 4 right 5 left_top 6 left_bottom 7 right_top 8 right_bottom
-    // 1 top
-    for(var i = x - 1; i >= 0;i--){
+    // 用来确定八个方向最后一颗棋子之后的位置
+    point.tNum    = 1  // 上
+    point.bNum    = 1  // 下
+    point.lNum    = 1  // 左
+    point.rNum    = 1  // 右
+    point.ltNum   = 1  // 左上
+    point.lbNum   = 1  // 左下
+    point.rtNum   = 1  // 右上
+    point.rbNum   = 1  // 右下
 
-    }
+    // 两端是否可以放棋子 默认不可以
+    point.isSpace_TOP          = false  // 上
+    point.isSpace_BOTTOM       = false  // 下
+    point.isSpace_LEFT         = false  // 左
+    point.isSpace_RIGHT        = false  // 右
+    point.isSpace_LEFT_TOP     = false  // 左上
+    point.isSpace_LEFT_BOTTOM  = false  // 左下
+    point.isSpace_RIGHT_TOP    = false  // 右上
+    point.isSpace_RIGHT_BOTTOM = false  // 右下
 
-}
+    point.tbNum   = 1 // 上下
+    point.lrNum   = 1 // 左右
+    point.ltrbNum = 1 // 左上右下
+    point.lbrtNum = 1 // 左下右上
 
-// 判断是输赢
-AI.isWin = function(x,y){
-
-    var len     = chessBoardArr.length
-    var type    = chessBoardArr[x][y]
-    var tbNum   = 1 // 上下
-    var lrNum   = 1 // 左右
-    var ltrbNum = 1 // 左上右下
-    var lbrtNum = 1 // 左下右上
-
-    var win = false
+    point.win     = false
 
     // 从八个方向来判断连续的数量 1 top 2 bottom 3 left 4 right 5 left_top 6 left_bottom 7 right_top 8 right_bottom
     // 1 上下
     for(var i = x - 1,j = y; i >= 0;i--){
-        if(chessBoardArr[i][j] == type) tbNum += 1;
+        if(chessBoardArr[i][j] != point.type) {
+            if(chessBoardArr[i][j] == 0){
+                point.isSpace_TOP = true;
+            }
+            break;
+        }
+        point.tNum += 1
+        point.tbNum += 1;
     }
-    for(var i = x + 1,j = y; i < len;i++){
-        if(chessBoardArr[i][j] == type) tbNum += 1;
+    for(var i = x + 1,j = y; i < point.len;i++){
+        if(chessBoardArr[i][j] != point.type){
+            if(chessBoardArr[i][j] == 0){
+                point.isSpace_BOTTOM = true;
+            }
+            break;
+        }
+        point.bNum += 1;
+        point.tbNum += 1;
     }
     // 1 左右
     for(var i = x,j = y -1 ; j >= 0;j--){
-        if(chessBoardArr[i][j] == type) lrNum += 1;
-        console.log('lrNum zuo ' + lrNum)
+        if(chessBoardArr[i][j] != point.type) {
+            if(chessBoardArr[i][j] == 0){
+                point.isSpace_LEFT = true;
+            }
+            break;
+        }
+        point.lNum += 1;
+        point.lrNum += 1;
 
     }
-    for(var i = x,j = y + 1; j < len;j++){
-        if(chessBoardArr[i][j] == type) lrNum += 1;
-        console.log('lrNum you ' + lrNum)
+    for(var i = x,j = y + 1; j < point.len;j++){
+        if(chessBoardArr[i][j] != point.type){
+            if(chessBoardArr[i][j] == 0){
+                point.isSpace_RIGHT = true;
+            }
+            break;
+        }
+        point.rNum += 1;
+        point.lrNum += 1;
+        //console.log('lrNum you ' + point.lrNum)
     }
     // 1 左上 右下
     for(var i = x - 1,j = y - 1; i >= 0 && j >= 0;i--,j--){
-        if(chessBoardArr[i][j] == type) ltrbNum += 1;
+        if(chessBoardArr[i][j] != point.type){
+            if(chessBoardArr[i][j] == 0){
+                point.isSpace_LEFT_TOP = true;
+            }
+            break;
+        }
+        point.ltNum += 1;
+        point.ltrbNum += 1;
     }
-    for(var i = x + 1,j = y + 1; i < len && j < len;i++,j++){
-        if(chessBoardArr[i][j] == type) ltrbNum += 1;
+    for(var i = x + 1,j = y + 1; i < point.len && j < point.len;i++,j++){
+        if(chessBoardArr[i][j] != point.type) {
+            if(chessBoardArr[i][j] == 0){
+                point.isSpace_RIGHT_BOTTOM = true;
+            }
+            break;
+        }
+        point.rbNum += 1;
+        point.ltrbNum += 1;
     }
     // 1 左下 右上
-    for(var i = x + 1,j = y - 1; i < len && j >= 0;i++,j--){
-        if(chessBoardArr[i][j] == type) lbrtNum += 1;
+    for(var i = x + 1,j = y - 1; i < point.len && j >= 0;i++,j--){
+        if(chessBoardArr[i][j] != point.type) {
+            if(chessBoardArr[i][j] == 0){
+                point.isSpace_LEFT_BOTTOM = true;
+            }
+            break;
+        }
+        point.lbNum += 1;
+        point.lbrtNum += 1;
     }
-    for(var i = x - 1,j = y + 1; i >= 0 && j < len;i--,j++){
-        if(chessBoardArr[i][j] == type) lbrtNum += 1;
+    for(var i = x - 1,j = y + 1; i >= 0 && j < point.len;i--,j++){
+        if(chessBoardArr[i][j] != point.type) {
+            if(chessBoardArr[i][j] == 0){
+                point.isSpace_RIGHT_TOP = true;
+            }
+            break;
+        }
+        point.rtNum += 1;
+        point.lbrtNum += 1;
     }
 
-    if(tbNum == 5 || lrNum == 5 || ltrbNum == 5 || lbrtNum == 5 ){
-        win = true
+    if(point.tbNum == 5 || point.lrNum == 5 || point.ltrbNum == 5 || point.lbrtNum == 5 ){
+        point.win = true
     }
-    return win;
+    return point;
 }
+
+// 下一步
+AI.nextStep = function (/*prevType,*/nextType) { // prevType 上一步的颜色，nextType 接下来的颜色，
+    var chessBoArrLen = chessBoardArr.length
+    var dxNum = 0
+    var dyNum = 0
+    var nextPoint = new Point()
+    var isPeace = false
+    var isNextType = false
+
+    for(var i = 0;i < chessBoArrLen;i++){
+        for(var j = 0; j < chessBoArrLen;j++){
+            // 先判断是否 有棋子 s
+            if(chessBoardArr[i][j] != 0 ){
+                var point = this.getChessNum(i,j)
+                if(point.tbNum == 4 || point.lrNum == 4 || point.ltrbNum == 4 || point.lbrtNum == 4){ // 如果四个方向任一方向有四个 s
+                    if(point.tbNum == 4 && (point.isSpace_TOP || point.isSpace_BOTTOM)){ // 如果是上下有四个
+                        dxNum = point.isSpace_BOTTOM ? point.bNum : -point.tNum;
+                        console.log('4ge top dxNum ' + dxNum + " dyNum " + dyNum)
+                    }
+                    else if(point.lrNum == 4 && (point.isSpace_LEFT || point.isSpace_RIGHT)){ // 如果是左右有四个
+                        dyNum = point.isSpace_LEFT ? -point.lNum : point.rNum;
+                        console.log('4ge left dxNum ' + dxNum + " dyNum " + dyNum)
+
+                    }
+                    else if(point.ltrbNum == 4 && (point.isSpace_LEFT_TOP || point.isSpace_RIGHT_BOTTOM)){ // 如果是左上右下有四个
+                        dxNum = dyNum = point.isSpace_LEFT_TOP ? -point.ltNum : point.rbNum;
+                        console.log('4ge left top dxNum ' + dxNum + " dyNum " + dyNum)
+
+                    }
+                    else if(point.lbrtNum == 4 && (point.isSpace_LEFT_BOTTOM || point.isSpace_RIGHT_TOP)){ // 如果是左下右上有四个
+                        dxNum = point.isSpace_LEFT_BOTTOM ? point.lbNum : -point.rtNum;
+                        dyNum = point.isSpace_LEFT_BOTTOM ? -point.lbNum : point.rtNum;
+                        console.log('4ge left bottom dxNum ' + dxNum + " dyNum " + dyNum)
+
+                    }
+
+
+                } // 如果四个方向任一方向有四个 e
+                else if(point.tbNum >= 3 || point.lrNum >= 3 || point.ltrbNum >= 3 || point.lbrtNum >= 3){ // 如果四个方向任一方向有三个 s
+                    if(point.tbNum == 3 && (point.isSpace_TOP && point.isSpace_BOTTOM)){ // 如果是上下有三个
+                        dxNum = (point.bNum == 0) ? -point.tNum : point.bNum ;
+                        console.log('3ge top dxNum ' + dxNum + " dyNum " + dyNum)
+
+                    }
+                    else if(point.lrNum == 3 && (point.isSpace_LEFT && point.isSpace_RIGHT)){ // 如果是左右有三个
+                        dyNum = (point.lNum == 0) ? point.rNum : -point.lNum ;
+                        console.log('3ge left dxNum ' + dxNum + " dyNum " + dyNum)
+
+                    }
+                    else if(point.ltrbNum == 3 && (point.isSpace_LEFT_TOP && point.isSpace_RIGHT_BOTTOM)){ // 如果是左上右下有三个
+                        dxNum = dyNum = (point.ltNum == 0) ? point.rbNum : -point.ltNum;
+                        console.log('3ge left top dxNum ' + dxNum + " dyNum " + dyNum)
+                    }
+                    else if(point.lbrtNum == 3 && (point.isSpace_LEFT_BOTTOM && point.isSpace_RIGHT_TOP)){ // 如果是左下右上有三个
+                        dxNum = (point.lbNum == 0) ? -point.rtNum : point.lbNum ;
+                        dyNum = (point.lbNum == 0) ? point.rtNum : -point.lbNum ;
+                        console.log('3ge left bottom dxNum ' + dxNum + " dyNum " + dyNum)
+
+                    }
+
+                } // 如果四个方向任一方向有三个 e
+                else {
+                    if(point.type == nextType && (point.isSpace_TOP || point.isSpace_BOTTOM || point.isSpace_LEFT || point.isSpace_RIGHT ||
+                        point.isSpace_LEFT_TOP || point.isSpace_LEFT_BOTTOM || point.isSpace_RIGHT_TOP || point.isSpace_RIGHT_BOTTOM)){
+                        if(point.isSpace_TOP || point.isSpace_BOTTOM){
+                            dxNum = point.isSpace_TOP ? -point.tNum : point.bNum;
+                            console.log('top point.tNum' + point.tNum+ ' point.bNum ' + point.bNum)
+                        }
+                        else if(point.isSpace_LEFT || point.isSpace_RIGHT){
+                            dyNum = point.isSpace_LEFT ? -point.lNum : point.rNum;
+                            console.log('left  point.lNum' + point.lNum+ ' point.rNum ' + point.rNum)
+                        }
+                        else if(point.isSpace_LEFT_TOP || point.isSpace_RIGHT_BOTTOM){
+                            dxNum = dyNum = point.isSpace_LEFT_TOP ? -point.ltNum : point.rbNum;
+                            console.log('left top point.ltNum' + point.ltNum+ ' point.rbNum ' + point.rbNum)
+                        }
+                        else if(point.isSpace_RIGHT_TOP || point.isSpace_LEFT_BOTTOM){
+                            dxNum = point.isSpace_RIGHT_TOP ? point.rtNum : -point.lbNum;
+                            dyNum = point.isSpace_RIGHT_TOP ? -point.rtNum : point.lbNum;
+                            console.log('left bottom point.lbNum' + point.lbNum+ ' point.rtNum ' + point.rtNum)
+                        }
+                    }
+                  /*  else{
+                        console.log('bufuhe')
+                    }*/
+                }
+                if(dxNum || dyNum){
+                    nextPoint = new Point(i + dxNum,j + dyNum)
+                    console.log(nextPoint)
+                    console.log('dxNum ' + dxNum + " dyNum " + dyNum)
+                    console.log('i ' + i + " j " + j + chessBoardArr[i][j])
+                    return nextPoint;
+                }
+                if( i == chessBoArrLen - 1 && j ==  chessBoArrLen - 1){
+                    console.log('bufuhe')
+                }
+
+            } // 有棋子 e
+
+        }
+    }
+    return nextPoint;
+}
+
+// 判断是输赢
+AI.isWin = function(x,y){
+    return this.getChessNum(x,y).win;
+}
+
 
 AI.init = function () {
 }
--- public/chess/js/chess.js
+++ public/chess/js/chess.js
@@ -1,113 +1,264 @@
 /**
- * Created by Administrator on 2016/3/31.
+ * Created by Administrator on 2016/4/1.
  */
-    var me    = true
-    var chessBoardArr = []  // 棋盘交叉点
-    for(var i = 0;i < 15;i++){
-        chessBoardArr[i] = []
-        for(var j = 0;j < 15;j++){
-            chessBoardArr[i][j] = 0
+var me    = true
+var chessBoardArr = []  // 棋盘交叉点
+var chess = document.getElementById('chess')
+var ctx   = chess.getContext('2d')
+var over  = false
+
+// 赢法数组
+var wins = []
+
+// 赢法统计数组
+var myWin       = []
+var computerWin = []
+
+for(var i = 0;i < 15;i++){
+    chessBoardArr[i] = []
+    for(var j = 0;j < 15;j++){
+        chessBoardArr[i][j] = 0
+    }
+}
+
+for(var i = 0;i<15;i++){
+    wins[i] =[]
+    for(var j = 0;j < 15;j++){
+        wins[i][j] = []
+    }
+}
+
+var count = 0 ;// 赢法数组的索引
+
+// 横线赢法
+for(var i = 0;i < 15;i++){
+    for(var j = 0;j < 11;j++){
+        for(var k = 0;k < 5;k++){
+            wins[i][j+k][count] = true
         }
+        count ++;
     }
+}
 
-    AI.init()
-
-    var chess = document.getElementById('chess')
-    var ctx   = chess.getContext('2d')
-
-    ctx.strokeStyle = '#bfbfbf'
-
-    // 画水印
-    var logo = new Image()
-    logo.src = '../public/chess/images/whiteBg.png'
-    logo.onload = function () {
-        ctx.drawImage(logo,0,0,450,450)
-        drawChessBoard()
-
-        //drawChessMan(7,7)
-        //drawChessMan(0,0)
-        //drawChessMan(AI.getFirstPoint(12,12).x,AI.getFirstPoint(12,12).y)
-
-        me = !me
-    }
-
-    var drawChessBoard = function () {
-        for(var i = 0; i < 15; i++){
-            // 画横线
-            ctx.moveTo(15 + i * 30,15)
-            ctx.lineTo(15 + i * 30,435)
-            ctx.stroke()
-
-            // 画纵线
-            ctx.moveTo(15,15 + i * 30)
-            ctx.lineTo(435,15 + i * 30)
-            ctx.stroke()
+// 竖线赢法
+for(var i = 0;i < 15;i++){
+    for(var j = 0;j < 11;j++){
+        for(var k = 0;k < 5;k++){
+            wins[j+k][i][count] = true
         }
+        count ++;
     }
+}
 
-    // i,j 表示当前棋子的索引，me 表示黑棋或者白棋
-    var oneStep = function (i, j, me) {
-
-        // 画棋子 画圆需要开始路径
-        ctx.beginPath()
-        ctx.arc(15 + i * 30,15 + j * 30,13,0,2 * Math.PI)
-        ctx.closePath()
-        //画棋子的渐变
-        var gradient = ctx.createRadialGradient(15 + i * 30 - 2,15 + j * 30 - 2,13,15 + i * 30 - 2,15 + j * 30 - 2,0)
-
-        if(me){ // 黑棋
-            gradient.addColorStop(0,'#0a0a0a')
-            gradient.addColorStop(1,'#636766')
-        }else{ // 白棋
-            gradient.addColorStop(0,'#d1d1d1')
-            gradient.addColorStop(1,'#f9f9f9')
+// 斜线赢法 左上右下
+for(var i = 0;i < 11;i++){
+    for(var j = 0;j < 11;j++){
+        for(var k = 0;k < 5;k++){
+            wins[i+k][j+k][count] = true
         }
-
-        ctx.fillStyle = gradient
-        ctx.fill()
+        count ++;
     }
+}
+// 斜线赢法 左下右上
+for(var i = 0;i <11 ;i++){
+    for(var j = 14;j > 3;j--){
+        for(var k = 0;k < 5;k++){
+            wins[i+k][j-k][count] = true
+        }
+        count ++;
+    }
+}
+
+console.log(count)
+
+// 初始化统计数组
+for(var i = 0;i < count;i++){
+    myWin[i]       = 0
+    computerWin[i] = 0
+}
+
+ctx.strokeStyle = '#bfbfbf'
+
+// 画水印
+var logo = new Image()
+logo.src = '../public/chess/images/whiteBg.png'
+logo.onload = function () {
+    ctx.drawImage(logo,0,0,450,450)
+    drawChessBoard()
+
+}
+var drawChessBoard = function () {
+    for(var i = 0; i < 15; i++){
+        // 画横线
+        ctx.moveTo(15 + i * 30,15)
+        ctx.lineTo(15 + i * 30,435)
+        ctx.stroke()
+
+        // 画纵线
+        ctx.moveTo(15,15 + i * 30)
+        ctx.lineTo(435,15 + i * 30)
+        ctx.stroke()
+    }
+}
+
+// i,j 表示当前棋子的索引，me 表示黑棋或者白棋
+var oneStep = function (i, j, me) {
+
+    // 画棋子 画圆需要开始路径
+    ctx.beginPath()
+    ctx.arc(15 + i * 30,15 + j * 30,13,0,2 * Math.PI)
+    ctx.closePath()
+    //画棋子的渐变
+    var gradient = ctx.createRadialGradient(15 + i * 30 - 2,15 + j * 30 - 2,13,15 + i * 30 - 2,15 + j * 30 - 2,0)
+
+    if(me){ // 黑棋
+        gradient.addColorStop(0,'#0a0a0a')
+        gradient.addColorStop(1,'#636766')
+    }else{ // 白棋
+        gradient.addColorStop(0,'#d1d1d1')
+        gradient.addColorStop(1,'#f9f9f9')
+    }
+
+    ctx.fillStyle = gradient
+    ctx.fill()
+}
 
 
 chess.onclick = function (e) {
-        var chessX = e.offsetX
-        var chessY = e.offsetY
 
-        var i = Math.floor(chessX / 30)
-        var j = Math.floor(chessY / 30)
+    if(over) return;
 
-        // 画棋子
-        drawChessMan(i,j)
-    }
+    if(!me) return;
 
-    var drawChessMan = function (x, y) {
+    var chessX = e.offsetX
+    var chessY = e.offsetY
 
-        // 没有棋子才能落子
-        if(chessBoardArr[x][y] == 0){
-            oneStep(x,y,me)
-            if(me){ //黑子
-                chessBoardArr[x][y] = 1
-            }else{
-                chessBoardArr[x][y] = 2
-            }
-            if(AI.isWin(x,y)){
-                var type = chessBoardArr[x][y]
-                if(type == 1){
-                    alert("黑棋胜 游戏结束")
-                    clearCanvasChess()
-                }
-                else{
-                    alert("白棋胜 游戏结束")
-                    clearCanvasChess()
+    var i = Math.floor(chessX / 30)
+    var j = Math.floor(chessY / 30)
+
+    // 画棋子
+    // 没有棋子才能落子
+    if(chessBoardArr[i][j] == 0){
+        oneStep(i,j,me)
+        chessBoardArr[i][j] = 1
+        for(var k = 0; k < count;k++){
+            if(wins[i][j][k]){
+                myWin[k] ++;
+                computerWin[k] = 6;
+                if(myWin[k] == 5){
+                    alert('你赢了！')
+                    over = true
                 }
             }
-            me = !me;
         }
+        if(!over){
+            me = !me;
+            computerAI()
+        }
+    }
+}
 
+var computerAI = function () {
+
+    // 得分数组
+    var myScore       = []
+    var computerScore = []
+
+    var max = 0 //用来保存最高分
+    var u = 0,v = 0 ;//用来保存最高分的坐标
+
+    for(var i = 0;i < 15;i++){
+        myScore[i]       = []
+        computerScore[i] = []
+        for(var j = 0;j < 15;j++){
+            myScore[i][j]       = 0
+            computerScore[i][j] = 0
+        }
     }
 
-    var clearCanvasChess = function () {
-        window.location.reload()
+    // 遍历棋盘
+    for(var i = 0;i < 15;i++){
+        for(var j = 0;j < 15;j++){
+            if(chessBoardArr[i][j] == 0){ // 如果没有棋子
+                // 循环比遍历赢法数组，判断在这个点落子有价值
+                for(var k = 0;k < count;k++){
+                    if(wins[i][j][k]){
+                        // 分值计算
+                        if(myWin[k] == 1){
+                            myScore[i][j] += 200
+                        }
+                        else if(myWin[k] == 2){
+                            myScore[i][j] += 400
+                        }
+                        else if(myWin[k] == 3){
+                            myScore[i][j] += 2000
+                        }
+                        else if(myWin[k] == 4){
+                            myScore[i][j] += 10000
+                        }
+
+                        //电脑
+                        if(computerWin[k] == 1){
+                            computerScore[i][j] += 220
+                        }
+                        else if(computerWin[k] == 2){
+                            computerScore[i][j] += 420
+                        }
+                        else if(computerWin[k] == 3){
+                            computerScore[i][j] += 2100
+                        }
+                        else if(computerWin[k] == 4){
+                            computerScore[i][j] += 20000
+                        }
+                    }
+                } // 遍历赢法数组 e
+
+                if(myScore[i][j] > max){
+                    max = myScore[i][j]
+                    u = i
+                    v = j
+                }
+                else if(myScore[i][j] == max){
+                    if(computerScore[i][j] > computerScore[u][v]){
+                        u = i
+                        v = j
+                    }
+                }
+
+                if(computerScore[i][j] > max){
+                    max = computerScore[i][j]
+                    u = i
+                    v = j
+                }
+                else if(computerScore[i][j] == max){
+                    if(myScore[i][j] > myScore[u][v]){
+                        u = i
+                        v = j
+                    }
+                }
+            }
+        }
+    } // 循环遍历 棋盘 e
+
+    oneStep(u,v,false)
+    chessBoardArr[u][v] = 2
+
+    for(var k = 0; k < count;k++){
+        if(wins[u][v][k]){
+            computerWin[k] ++;
+            myWin[k] = 6;
+            if(computerWin[k] == 5){
+                alert('电脑赢了！')
+                over = true
+            }
+        }
     }
+    if(!over){
+        me = !me;
+    }
+
+}
+
 
 
 
